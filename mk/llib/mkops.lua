6f3db653842885fb97362f40b114963110d85d1e
Mirmik <Mirage666@yandex.ru>
2016-05-30 18:37:52 +0300

    third

--- mk/llib/mkops.lua
+++ mk/llib/mkops.lua
@@ -1,4 +1,6 @@
 mk = {}
+local os = require("os")
+local lfs = require("lfs")
 
 function mk.rule_build(container, prototype)
 	local lp
@@ -27,6 +29,20 @@ function mk.use_rule_list(rule,src,tgt)
 	for i = 1, #src do
 		mk.use_rule_simple(rule,src[i],tgt[i])
 	end
+end
+
+function mk.prepare_changed_only_list(srclist, tgtlist)
+	local _srcl = {}
+	local _tgtl = {}
+	for i = 1, #srclist do
+		if (not (paths.exists(tgtlist[i]))) or (lfs.attributes(srclist[i],"modification") 
+			> lfs.attributes(tgtlist[i],"modification"))
+		then
+		table.insert(_srcl, srclist[i])
+		table.insert(_tgtl, tgtlist[i])
+		end
+	end
+	return _srcl, _tgtl
 end
 
 function mk.prepare_srctgt_lists(list, tgtexp, srcprefix, tgtprefix)
